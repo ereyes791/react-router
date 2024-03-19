40945d9bd87768e05ceff2d31872dafa0baac0cc
Esteban S <esteban.samuelreyes@gmail.com>
2024-03-18 20:01:11 -0500

    router

---
+++ react-router/src/components/Blue.jsx
@@ -0,0 +1,7 @@
+export default function Blue() {
+    return (
+        <div className="blue">
+
+        </div>
+    )
+}
---
+++ react-router/src/components/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(){
+    return(
+        <h1 className="home">Home</h1>
+    )
+}
